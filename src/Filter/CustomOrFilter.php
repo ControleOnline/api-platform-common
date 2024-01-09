3554021d6dbdac632b4232141ebd5c56e9ee8e39
Luiz Kim <luiz.kim@controleonline.com>
2024-01-08 21:17:06 -0300

    Update

--- src/Filter/CustomOrFilter.php
+++ src/Filter/CustomOrFilter.php
@@ -2,18 +2,33 @@
 
 namespace ControleOnline\Filter;
 
-
 use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
 use Doctrine\ORM\QueryBuilder;
-use Symfony\Component\PropertyInfo\Type;
-use ApiPlatform\Api\FilterInterface;
 
-final class CustomOrFilter extends AbstractFilter
+class CustomOrFilter extends AbstractFilter
 {
+    public function getDescription(string $resourceClass): array
+    {
+        return [
+            'search' => [
+                'property' => null,
+                'type' => 'string',
+                'required' => false,
+                'openapi' => [
+                    'description' => 'Search across multiple fields',
+                ],
+            ]
+        ];
+    }
+
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
     {
+        if ($property !== 'search') {
+            return;
+        }
+
         $rootAlias = $queryBuilder->getRootAliases()[0];
         foreach (array_keys($this->getProperties()) as $prop) { // we use array_keys() because getProperties() returns a map of property => strategy
             if (!$this->isPropertyEnabled($prop, $resourceClass) || !$this->isPropertyMapped($prop, $resourceClass)) {
@@ -24,31 +39,5 @@ final class CustomOrFilter extends AbstractFilter
                 ->orWhere(sprintf('%s.%s LIKE :%s', $rootAlias, $prop, $parameterName))
                 ->setParameter($parameterName, "%" . $value . "%");
         }
-    }
-
-    // This function is only used to hook in documentation generators (supported by Swagger and Hydra)
-    public function getDescription(string $resourceClass): array
-    {
-        if (!$this->properties) {
-            return [];
-        }
-
-        $description = [];
-        foreach ($this->properties as $property => $strategy) {
-            $description[$property] = [
-                'property' => $property,
-                'type' => Type::BUILTIN_TYPE_STRING,
-                'required' => false,
-                'description' => 'Filter using a regex. This will appear in the OpenApi documentation!',
-                'openapi' => [
-                    'example' => 'Custom example that will be in the documentation and be the default value of the sandbox',
-                    'allowReserved' => false, // if true, query parameters will be not percent-encoded
-                    'allowEmptyValue' => true,
-                    'explode' => false, // to be true, the type must be Type::BUILTIN_TYPE_ARRAY, ?product=blue,green will be ?product=blue&product=green
-                ],
-            ];
-        }
-
-        return $description;
     }
 }
