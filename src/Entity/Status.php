37073efa8c1b6d5215e47fcaefee1234b10ec750
Luiz Kim <luiz.kim@controleonline.com>
2024-01-14 15:02:38 -0300

    Update

--- src/Entity/Status.php
+++ src/Entity/Status.php
@@ -10,6 +10,7 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\EntityListeners ({App\Listener\LogListener::class})
