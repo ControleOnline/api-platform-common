d8855f4932f30db920e0128315ea392f6b228fa7
Luiz Kim <luiz.kim@controleonline.com>
2024-07-18 01:52:16 -0300

    Update

--- src/Service/PusherService.php
+++ src/Service/PusherService.php
@@ -3,27 +3,28 @@
 namespace ControleOnline\Service;
 
 use ControleOnline\Message\Notification;
-use stdClass;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 class PusherService
 {
 
-    public function __construct(
-        private MessageBusInterface $messageBus
-    ) {
+    public function __construct(private MessageBusInterface $messageBus, private  LoggerInterface $logger)
+    {
     }
 
     public function push(array $data, string $topic)
     {
-
-        return;
         try {
+            $this->logger->info('Attempting to push message', ['data' => $data, 'topic' => $topic]);
+
             $message = new Notification($data, $topic);
             $this->messageBus->dispatch($message);
+
+            $this->logger->info('Message dispatched successfully');
         } catch (\Exception $e) {
-            // Handle exception, e.g., log the error
-            throw $e; // Or rethrow for further handling
+            $this->logger->error('Failed to dispatch message', ['exception' => $e]);
+            throw $e;
         }
     }
 }
