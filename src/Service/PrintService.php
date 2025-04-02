954afb8682deef95bc14ee1d1bb6b146e7e2e18a
Luiz Kim <luiz.kim@controleonline.com>
2025-04-02 14:57:20 -0300

    Update

--- src/Service/PrintService.php
+++ src/Service/PrintService.php
@@ -11,7 +11,7 @@ class PrintService
 {
     private $noQueue = 'Sem fila definida';
     private $initialSpace = 8;
-    private $totalChars = 80;
+    private $totalChars = 40;
     private $text = '';
 
     public function __construct(
