838c5abf049d9b0ece9b8655b72f038b7d5ad9b4
Luiz Kim <luiz.kim@controleonline.com>
2025-04-03 04:00:31 -0300

    Update

--- src/Entity/Device.php
+++ src/Entity/Device.php
@@ -151,7 +151,7 @@ class Device
 
     public function getConfigs($decode = false)
     {
-        return $decode ? json_decode((is_array($this->configs) ? json_encode($this->configs) : $this->configs)) : $this->configs;
+        return $decode ? json_decode((is_array($this->configs) ? json_encode($this->configs) : $this->configs), true) : $this->configs;
     }
 
 
