a38900c94d7d863cc89d622bbc2fa2b4362ee15a
Luiz Kim <luiz.kim@controleonline.com>
2025-04-04 15:32:47 -0300

    Update

--- src/Controller/AddDeviceConfigAction.php
+++ src/Controller/AddDeviceConfigAction.php
@@ -34,7 +34,8 @@ class AddDeviceConfigAction
       $people = $this->manager->getRepository(People::class)->find(preg_replace("/[^0-9]/", "", $json['people']));
       $configs = json_decode($json['configs'], true);
       $device_config = $this->deviceService->addDeviceConfigs($people, $configs, $json['device_name']);
-      return new JsonResponse($this->hydratorService->item(DeviceConfig::class, $device_config->getId(), 'device_config:read'), Response::HTTP_OK);
+      return new JsonResponse(['teste']);
+      //return new JsonResponse($this->hydratorService->item(DeviceConfig::class, $device_config->getId(), 'device_config:read'), Response::HTTP_OK);
     } catch (Exception $e) {
       return new JsonResponse($this->hydratorService->error($e));
     }
