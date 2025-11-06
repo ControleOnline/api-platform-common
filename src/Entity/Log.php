098342556670cdcba0e52f65b32ee5ed282441be
Luiz Kim <luiz.kim@controleonline.com>
2025-11-06 18:28:17 -0300

    Removing Log Listener

--- src/Entity/Log.php
+++ src/Entity/Log.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ControleOnline\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -21,9 +22,95 @@ class Log
     #[ORM\ManyToOne(targetEntity: User::class)]
     private ?User $user = null;
 
-    public function getId(): ?int { return $this->id; }
-    public function setAction(string $a): self { $this->action = $a; return $this; }
-    public function setClass(string $c): self { $this->class = $c; return $this; }
-    public function setObject(string $o): self { $this->object = $o; return $this; }
-    public function setUser(?User $u): self { $this->user = $u; return $this; }
+
+
+    /**
+     * Get the value of id
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of action
+     */
+    public function getAction(): string
+    {
+        return $this->action;
+    }
+
+    /**
+     * Set the value of action
+     */
+    public function setAction(string $action): self
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of class
+     */
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+
+    /**
+     * Set the value of class
+     */
+    public function setClass(string $class): self
+    {
+        $this->class = $class;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of object
+     */
+    public function getObject(): string
+    {
+        return $this->object;
+    }
+
+    /**
+     * Set the value of object
+     */
+    public function setObject(string $object): self
+    {
+        $this->object = $object;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user
+     */
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the value of user
+     */
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
 }
