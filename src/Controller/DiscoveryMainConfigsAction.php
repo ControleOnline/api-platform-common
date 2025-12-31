db4c4ab639b5b833b4e855d5a91b24e1f05f99f1
Luiz Kim <luiz.kim@controleonline.com>
2025-12-31 14:09:06 -0300

    Fix

--- src/Controller/DiscoveryMainConfigsAction.php
+++ src/Controller/DiscoveryMainConfigsAction.php
@@ -3,6 +3,7 @@
 namespace ControleOnline\Controller;
 
 use ControleOnline\Entity\Config;
+use ControleOnline\Entity\Device;
 use ControleOnline\Entity\People;
 use ControleOnline\Service\ConfigService;
 use ControleOnline\Service\HydratorService;
@@ -23,7 +24,7 @@ class DiscoveryMainConfigsAction
   {
     try {
       $json = json_decode($request->getContent(), true);
-      $device = $this->manager->getRepository(People::class)->findOneBy(['device' => $request->headers->get('device')]);
+      $device = $this->manager->getRepository(Device::class)->findOneBy(['device' => $request->headers->get('device')]);
       $people = $this->manager->getRepository(People::class)->find(preg_replace("/[^0-9]/", "", $json['people']));
       $configs = $this->configService->discoveryMainConfigs($people, $device);
       return new JsonResponse($this->hydratorService->collectionData($configs, Config::class, 'config:read'));
