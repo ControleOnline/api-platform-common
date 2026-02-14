98df4b1abaf5e008f8f6bb0bd18cdafe46bcabff
Luiz Kim <luiz.kim@controleonline.com>
2026-02-14 14:52:43 -0300

    Debug

--- src/Service/ExtraDataService.php
+++ src/Service/ExtraDataService.php
@@ -50,7 +50,11 @@ class ExtraDataService
     {
         $class = $this->getEntityName($entity);
         $extraFields = $this->discoveryExtraFields($fieldName, $context, '{}');
-
+        print_r([
+            'extra_fields' => $extraFields,
+            'entity_name' => $class->getShortName(),
+            'value' => $code
+        ]);
         $extraData = $this->manager->getRepository(ExtraData::class)->findOneBy([
             'extra_fields' => $extraFields,
             'entity_name' => $class->getShortName(),
