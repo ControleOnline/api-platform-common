9b9c8369e44bfe43b8c5c14dea5c01c1f98a68c5
Luiz Kim <luiz.kim@controleonline.com>
2026-03-09 21:26:12 -0300

    StatusService

--- src/Controller/FileUploadController.php
+++ src/Controller/FileUploadController.php
@@ -7,9 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use ControleOnline\Entity\File;
 use ControleOnline\Entity\People;
+use ControleOnline\Entity\Import;
 use ControleOnline\Service\FileService;
 use Doctrine\ORM\EntityManagerInterface;
 use ControleOnline\Service\HydratorService;
+use ControleOnline\Service\StatusService;
 use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -19,18 +21,18 @@ class FileUploadController
     public function __construct(
         private EntityManagerInterface $em,
         private HydratorService $hydratorService,
-        private FileService $fileService
+        private FileService $fileService,
+        private StatusService $statusService
     ) {}
 
     public function __invoke(Request $request): Response
     {
 
         try {
+
             $file = $request->files->get('file');
             $people_id = $request->request->get('people');
             $context = $request->request->get('context');
-            //$file_id = $request->request->get('id');
-
 
             if (!$file) {
                 throw new BadRequestHttpException('No file provided');
@@ -40,16 +42,51 @@ class FileUploadController
             $fileType = explode('/', $file->getClientMimeType());
             $originalFilename = $file->getClientOriginalName();
 
-            //if ($file_id)
-            //    $fileEntity = $this->em->getRepository(File::class)->find($file_id);
-            //if (!$fileEntity)
-            $people = $this->em->getRepository(People::class)->find($people_id);
+            $people = null;
 
-            $fileEntity = $this->fileService->addFile($people, $content, $context, $originalFilename, $fileType[0], $fileType[1]);
+            if ($people_id) {
+                $people = $this->em->getRepository(People::class)->find($people_id);
+            }
 
-            return new JsonResponse($this->hydratorService->data($fileEntity, 'file:read'), Response::HTTP_CREATED);
+            $fileEntity = $this->fileService->addFile(
+                $people,
+                $content,
+                $context,
+                $originalFilename,
+                $fileType[0],
+                $fileType[1]
+            );
+
+            $extension = strtolower(pathinfo($originalFilename, PATHINFO_EXTENSION));
+
+            if ($extension === 'csv') {
+
+                $import = new Import();
+
+                $import->setFile($fileEntity);
+                $import->setPeople($people);
+                $import->setFileFormat('csv');
+                $import->setImportType($context ?: 'people');
+                $import->setStatus(
+                    $this->statusService->discoveryStatus(
+                        'open',
+                        'open',
+                        'integration'
+                    )
+                );
+                $this->em->persist($import);
+                $this->em->flush();
+            }
+
+            return new JsonResponse(
+                $this->hydratorService->data($fileEntity, 'file:read'),
+                Response::HTTP_CREATED
+            );
         } catch (Exception $e) {
-            return new JsonResponse($this->hydratorService->error($e));
+
+            return new JsonResponse(
+                $this->hydratorService->error($e)
+            );
         }
     }
 }
