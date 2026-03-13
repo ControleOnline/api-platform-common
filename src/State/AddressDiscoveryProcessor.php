9e89225cd72eeb4f622e6f55b30a6b5f625c55a7
Luiz Kim <luiz.kim@controleonline.com>
2026-03-13 17:23:37 -0300

    Address

--- src/State/AddressDiscoveryProcessor.php
+++ src/State/AddressDiscoveryProcessor.php
@@ -5,7 +5,6 @@ namespace ControleOnline\State;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use ControleOnline\Service\AddressService;
-use ControleOnline\Entity\Address;
 use ControleOnline\Entity\People;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -22,25 +21,31 @@ class AddressDiscoveryProcessor implements ProcessorInterface
         $input = $context['request']->toArray();
 
         $people = null;
+
         if (!empty($input['people'])) {
+            $peopleId = $input['people'];
+
+
+            $peopleId = (int) str_replace('/people/', '', $peopleId);
+
             $people = $this->manager
                 ->getRepository(People::class)
-                ->find($input['people']);
+                ->find($peopleId);
         }
 
         return $this->addressService->discoveryAddress(
             $people,
-            $input['postalCode'] ?? throw new BadRequestHttpException('postalCode required'),
-            $input['streetNumber'] ?? throw new BadRequestHttpException('streetNumber required'),
-            $input['streetName'] ?? throw new BadRequestHttpException('streetName required'),
+            $input['cep'] ?? throw new BadRequestHttpException('cep required'),
+            $input['number'] ?? throw new BadRequestHttpException('number required'),
+            $input['street'] ?? throw new BadRequestHttpException('street required'),
             $input['district'] ?? throw new BadRequestHttpException('district required'),
             $input['city'] ?? throw new BadRequestHttpException('city required'),
-            $input['uf'] ?? throw new BadRequestHttpException('uf required'),
-            $input['countryCode'] ?? throw new BadRequestHttpException('countryCode required'),
+            $input['state'] ?? throw new BadRequestHttpException('state required'),
+            $input['country'] ?? throw new BadRequestHttpException('country required'),
             $input['complement'] ?? null,
             $input['latitude'] ?? 0,
             $input['longitude'] ?? 0,
-            $input['nickName'] ?? 'Default'
+            $input['nickname'] ?? 'Default'
         );
     }
 }
