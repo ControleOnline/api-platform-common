9d584598fb2724a26b3352002ae38b3c229923ad
Luiz Kim <luiz.kim@controleonline.com>
2026-04-08 15:41:59 -0300

    Refatora o método securityFilter para incluir verificação de empresas e filtro de pessoas

--- src/Service/DeviceConfigService.php
+++ src/Service/DeviceConfigService.php
@@ -3,15 +3,34 @@
 namespace ControleOnline\Service;
 
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class DeviceConfigService
 {
+    private $request;
+
     public function __construct(
-        private PeopleService $peopleService
-    ) {}
+        private PeopleService $peopleService,
+        RequestStack $requestStack,
+    ) {
+        $this->request = $requestStack->getCurrentRequest();
+    }
 
     public function securityFilter(QueryBuilder $queryBuilder, $resourceClass = null, $applyTo = null, $rootAlias = null): void
     {
-        $this->peopleService->checkCompany('people', $queryBuilder, $resourceClass, $applyTo, $rootAlias);
+        $companies = $this->peopleService->getMyCompanies();
+
+        if (empty($companies)) {
+            $queryBuilder->andWhere('1 = 0');
+            return;
+        }
+
+        $queryBuilder->andWhere(sprintf('%s.people IN(:companies)', $rootAlias));
+        $queryBuilder->setParameter('companies', $companies);
+
+        if ($people = $this->request?->query->get('people', null)) {
+            $queryBuilder->andWhere(sprintf('%s.people IN(:people)', $rootAlias));
+            $queryBuilder->setParameter('people', preg_replace("/[^0-9]/", "", $people));
+        }
     }
 }
