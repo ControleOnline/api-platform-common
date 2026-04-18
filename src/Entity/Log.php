add52780d0f55ddc852a1a4cf4f7b275cabb4bca
Luiz Kim <luiz.kim@controleonline.com>
2026-04-18 03:13:36 -0300

    Refatora a entidade Log para incluir novos campos e métodos, e adiciona LogService com lógica de filtragem de segurança.

--- src/Entity/Log.php
+++ src/Entity/Log.php
@@ -2,45 +2,76 @@
 
 namespace ControleOnline\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity]
+#[ORM\Table(name: 'log')]
+#[ApiResource(
+    operations: [
+        new GetCollection(
+            security: "is_granted('ROLE_CLIENT')",
+        ),
+    ],
+    formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => ['text/csv']],
+    normalizationContext: ['groups' => ['log:read']],
+    security: "is_granted('ROLE_CLIENT')",
+    order: ['createdAt' => 'DESC', 'id' => 'DESC']
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'id' => 'exact',
+    'type' => 'exact',
+    'action' => 'exact',
+    'class' => 'exact',
+    'row' => 'exact',
+])]
+#[ApiFilter(OrderFilter::class, properties: [
+    'createdAt',
+    'id',
+])]
 class Log
 {
     #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: 'integer')]
+    #[Groups(['log:read'])]
     private ?int $id = null;
 
     #[ORM\Column(type: 'string', length: 50, options: ['default' => 'entity'])]
+    #[Groups(['log:read'])]
     private string $type = 'entity';
 
     #[ORM\Column(type: 'string')]
+    #[Groups(['log:read'])]
     private string $action;
 
-    #[ORM\Column(type: 'string')]
+    #[ORM\Column(name: 'class', type: 'string')]
+    #[Groups(['log:read'])]
     private string $class;
 
-    #[ORM\Column(type: 'integer')]
-    private string $row;
+    #[ORM\Column(name: 'row', type: 'integer')]
+    #[Groups(['log:read'])]
+    private int $row;
 
     #[ORM\Column(type: 'text')]
     private string $object;
 
+    #[ORM\Column(name: 'created_at', type: 'datetime_immutable', insertable: false, updatable: false)]
+    #[Groups(['log:read'])]
+    private ?DateTimeImmutable $createdAt = null;
+
     #[ORM\ManyToOne(targetEntity: User::class, cascade: ['persist'])]
     private ?User $user = null;
 
-
-
-    /**
-     * Get the value of id
-     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * Set the value of id
-     */
     public function setId(?int $id): self
     {
         $this->id = $id;
@@ -48,17 +79,11 @@ class Log
         return $this;
     }
 
-    /**
-     * Get the value of type
-     */
     public function getType(): string
     {
         return $this->type;
     }
 
-    /**
-     * Set the value of type
-     */
     public function setType(string $type): self
     {
         $this->type = $type;
@@ -66,17 +91,11 @@ class Log
         return $this;
     }
 
-    /**
-     * Get the value of action
-     */
     public function getAction(): string
     {
         return $this->action;
     }
 
-    /**
-     * Set the value of action
-     */
     public function setAction(string $action): self
     {
         $this->action = $action;
@@ -84,17 +103,11 @@ class Log
         return $this;
     }
 
-    /**
-     * Get the value of class
-     */
     public function getClass(): string
     {
         return $this->class;
     }
 
-    /**
-     * Set the value of class
-     */
     public function setClass(string $class): self
     {
         $this->class = $class;
@@ -102,17 +115,11 @@ class Log
         return $this;
     }
 
-    /**
-     * Get the value of object
-     */
     public function getObject(): string
     {
         return $this->object;
     }
 
-    /**
-     * Set the value of object
-     */
     public function setObject(string $object): self
     {
         $this->object = $object;
@@ -120,17 +127,11 @@ class Log
         return $this;
     }
 
-    /**
-     * Get the value of user
-     */
     public function getUser(): ?User
     {
         return $this->user;
     }
 
-    /**
-     * Set the value of user
-     */
     public function setUser(?User $user): self
     {
         $this->user = $user;
@@ -138,21 +139,44 @@ class Log
         return $this;
     }
 
-    /**
-     * Get the value of row
-     */
-    public function getRow(): string
+    public function getRow(): int
     {
         return $this->row;
     }
 
-    /**
-     * Set the value of row
-     */
-    public function setRow(string $row): self
+    public function setRow(int $row): self
     {
         $this->row = $row;
 
         return $this;
+    }
+
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    #[Groups(['log:read'])]
+    public function getPayload(): array
+    {
+        $decoded = json_decode($this->object, true);
+
+        return is_array($decoded) ? $decoded : [];
+    }
+
+    #[Groups(['log:read'])]
+    public function getUserDisplayName(): ?string
+    {
+        return $this->user?->getPeople()?->getAlias()
+            ?: $this->user?->getPeople()?->getName()
+            ?: $this->user?->getUsername();
+    }
+
+    #[Groups(['log:read'])]
+    public function getEntityShortName(): string
+    {
+        $classParts = explode('\\', $this->class);
+
+        return end($classParts) ?: $this->class;
     }
 }
---
+++ src/Service/LogService.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace ControleOnline\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class LogService
+{
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private ContainerInterface $container,
+        private RequestStack $requestStack,
+    ) {}
+
+    public function securityFilter(QueryBuilder $queryBuilder, $resourceClass = null, $applyTo = null, $rootAlias = null): void
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        $targetClass = trim((string) $request?->query->get('class', ''));
+        $targetRow = (int) preg_replace('/\D+/', '', (string) $request?->query->get('row', ''));
+
+        if (
+            $targetClass === ''
+            || $targetRow <= 0
+            || !class_exists($targetClass)
+            || !preg_match('/^ControleOnline\\\\Entity\\\\[A-Za-z0-9_\\\\]+$/', $targetClass)
+        ) {
+            $this->denyAll($queryBuilder, $rootAlias);
+            return;
+        }
+
+        $queryBuilder->andWhere(sprintf('%s.class = :log_target_class', $rootAlias));
+        $queryBuilder->andWhere(sprintf('%s.row = :log_target_row', $rootAlias));
+        $queryBuilder->setParameter('log_target_class', $targetClass);
+        $queryBuilder->setParameter('log_target_row', $targetRow);
+
+        $serviceName = str_replace('Entity', 'Service', $targetClass) . 'Service';
+        if (!$this->container->has($serviceName)) {
+            return;
+        }
+
+        $service = $this->container->get($serviceName);
+        if (!method_exists($service, 'securityFilter')) {
+            return;
+        }
+
+        $subQueryBuilder = $this->manager->createQueryBuilder()
+            ->select('log_security_entity.id')
+            ->from($targetClass, 'log_security_entity')
+            ->andWhere('log_security_entity.id = :log_target_row');
+
+        $service->securityFilter(
+            $subQueryBuilder,
+            $targetClass,
+            'collection',
+            'log_security_entity'
+        );
+
+        foreach ($subQueryBuilder->getParameters() as $parameter) {
+            $queryBuilder->setParameter(
+                $parameter->getName(),
+                $parameter->getValue(),
+                $parameter->getType()
+            );
+        }
+
+        $queryBuilder->andWhere(
+            $queryBuilder->expr()->in(
+                sprintf('%s.row', $rootAlias),
+                $subQueryBuilder->getDQL()
+            )
+        );
+    }
+
+    private function denyAll(QueryBuilder $queryBuilder, ?string $rootAlias): void
+    {
+        $queryBuilder->andWhere(sprintf('%s.id IS NULL', $rootAlias));
+    }
+}
