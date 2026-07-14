f372b98b71b8cc43e894db9790dceefc4f1a450a
Alexandre de Silva Cunha <alemac@mac.com>
2026-07-13 21:38:04 -0300

    fix para midia pública

--- src/Service/FileService.php
+++ src/Service/FileService.php
@@ -4,6 +4,7 @@ namespace ControleOnline\Service;
 
 use ControleOnline\Entity\File;
 use ControleOnline\Entity\People;
+use ControleOnline\Entity\PeopleMedia;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
@@ -22,13 +23,49 @@ class FileService
   public function getFileUrl(People $people): ?array
   {
     if ($people->getImage() instanceof File)
-      return [
-        'id'     => $people->getImage()->getId(),
-        'domain' => $this->domainService->getMainDomain(),
-        'url'    => '/files/' . $people->getImage()->getId() . '/download'
-      ];
+      return $this->buildFileUrl($people->getImage());
 
     return null;
+  }
+
+  public function getPeopleMediaFileUrl(People $people, string $mediaType): ?array
+  {
+    $resolvedMediaType = strtolower(trim($mediaType));
+    if ($resolvedMediaType === '') {
+      return null;
+    }
+
+    $peopleMedia = $this->manager
+      ->getRepository(PeopleMedia::class)
+      ->createQueryBuilder('peopleMedia')
+      ->innerJoin('peopleMedia.mediaType', 'mediaType')
+      ->andWhere('peopleMedia.people = :people')
+      ->andWhere('mediaType.type = :type')
+      ->setParameter('people', $people)
+      ->setParameter('type', $resolvedMediaType)
+      ->orderBy('peopleMedia.id', 'DESC')
+      ->setMaxResults(1)
+      ->getQuery()
+      ->getOneOrNullResult();
+
+    if (!$peopleMedia instanceof PeopleMedia || !$peopleMedia->getFile() instanceof File) {
+      return null;
+    }
+
+    return $this->buildFileUrl($peopleMedia->getFile());
+  }
+
+  public function buildFileUrl(?File $file): ?array
+  {
+    if (!$file instanceof File) {
+      return null;
+    }
+
+    return [
+      'id'     => $file->getId(),
+      'domain' => $this->domainService->getMainDomain(),
+      'url'    => '/files/' . $file->getId() . '/download'
+    ];
   }
 
   public function addFile(?People $people, string  $content, string $context, ?string $fileName = null, ?string $fileType = null, ?string $extension = null): File
