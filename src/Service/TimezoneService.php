13ac5022e577a6d49785c4c80e1697a1215abafa
luizkim <luizkim@users.noreply.github.com>
2026-07-23 17:35:57 -0300

    Add timezone service

---
+++ src/Service/TimezoneService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ControleOnline\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+class TimezoneService
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {}
+
+    public function applyForUser($user): void
+    {
+        if (!$user || !method_exists($user, 'getTimezone')) {
+            return;
+        }
+
+        $timezone = $user->getTimezone()?->getName() ?? 'UTC';
+
+        $this->applyTimezone($timezone);
+    }
+
+    public function applyTimezone(string $timezone): void
+    {
+        date_default_timezone_set($timezone);
+
+        if (!filter_var($_ENV['MYSQL_USER_TIMEZONE'] ?? false, FILTER_VALIDATE_BOOLEAN)) {
+            return;
+        }
+
+        $this->entityManager
+            ->getConnection()
+            ->executeStatement('SET time_zone = :timezone', ['timezone' => $this->getMysqlTimezoneOffset($timezone)]);
+    }
+
+    private function getMysqlTimezoneOffset(string $timezone): string
+    {
+        $dateTimeZone = new \DateTimeZone($timezone);
+        $offset = $dateTimeZone->getOffset(new \DateTimeImmutable('now', $dateTimeZone));
+        $sign = $offset < 0 ? '-' : '+';
+        $offset = abs($offset);
+
+        return sprintf('%s%02d:%02d', $sign, intdiv($offset, 3600), intdiv($offset % 3600, 60));
+    }
+}
