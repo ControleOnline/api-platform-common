84586ab91cf71939fc0f00deef56ce245cb487d6
Luiz Kim <luiz.kim@controleonline.com>
2026-08-12 00:29:38 -0300

    Revert "Merge branch 'task-16-address-geo-common' into staging"
    This reverts commit f2585ffc26dd35c0163081537753ad8b8fbd08dd, reversing
    changes made to f75d079c7d574cf18b9b32b07a44f5588d6a3cd1.

--- src/Library/Company/CompanyService.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace ControleOnline\Library\Company;
-
-use ControleOnline\Library\Company\Entity\Company;
-
-interface CompanyService
-{
-  public function query(string $cnpj) :Company;
-}
--- src/Library/Company/Exception/InvalidParameterException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace ControleOnline\Library\Company\Exception;
-
-final class InvalidParameterException extends \Exception implements ExceptionInterface
-{
-}
